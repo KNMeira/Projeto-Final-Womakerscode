c0b4de6c6bf3d5529c42803be5120798f9be7de6
Beatriz Alencar Kuriki <beatriz.alencar@sesatech.com.br>
2024-04-21 08:31:48 -0300

    update admin.py

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,9 +1,5 @@
-@admin.register(Cliente)
-class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'sobrenome', 'email', 'cpf')
-    fields = [('nome', 'sobrenome'), 'email', ('cpf')]  
+from django.contrib import admin
+from .models import Cliente,Carro
 
-@admin.register(Carro)
-class CarroAdmin(admin.ModelAdmin):
-    list_display = ('carro', 'placa', 'ano', 'cliente')
-    list_filter = ('ano', 'cliente')
+admin.site.register(Cliente)
+admin.site.register(Carro)
